226b5027ad8a7b084314bbfbf62025e05fc4ee2b
kaine119 <muikaien1@gmail.com>
2020-07-21 00:18:24 +0800

    Add unit tests

---
+++ DatabaseTest/KeyBunchTest.cs
@@ -0,0 +1,33 @@
+using Database.DatabaseTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DatabaseTest
+{
+    [TestClass]
+    public class KeyBunchTest
+    {
+        private KeyBunch TestKeyBunch;
+        private List<Person> TestAuthorizedPersonnel = new List<Person> {
+          new Person { NRIC = "001A", Name = "Alice Tan", Rank = Rank.PTE, ContactNumber = "90123456" },
+          new Person { NRIC = "002B", Name = "Bob Lee", Rank = Rank.PTE, ContactNumber = "90123456" },
+          new Person { NRIC = "003C", Name = "Charlie Chan", Rank = Rank.PTE, ContactNumber = "90123456" },
+        };
+
+        [TestMethod]
+        public void FindPersonnel_CorrectlyReturnsPersonnel()
+        {
+            TestKeyBunch = new KeyBunch
+            {
+                AuthorizedPersonnel = TestAuthorizedPersonnel
+            };
+            
+            List<Person> queryForEveryone = TestKeyBunch.FindPersonnel("00"); // everyone on the list has an NRIC starting with 00
+            Assert.AreEqual(queryForEveryone.Count, 3);
+
+            List<Person> queryForAlice = TestKeyBunch.FindPersonnel("Alice");
+            Assert.AreEqual(1, queryForAlice.Count);
+            Assert.AreEqual("Alice Tan", queryForAlice[0].Name);
+        }
+    }
+}
---
+++ DatabaseTest/LogEntryTest.cs
@@ -0,0 +1,37 @@
+﻿using Database.DatabaseTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace DatabaseTest
+{
+    [TestClass]
+    public class LogEntryTest
+    {
+        [TestMethod]
+        public void IsKeyReturned_CorrectlyReturns()
+        {
+            LogEntry unreturnedKey = new LogEntry
+            {
+                DateTimeIssued = DateTime.Now,
+                PersonDrawingKey = new Person { Name = "Drawer", ContactNumber = "90123456", NRIC = "100A", Rank = Rank.PTE },
+                PersonIssuingKey = new Person { Name = "Issuer", ContactNumber = "90123456", NRIC = "200B", Rank = Rank.CFC }
+            };
+            Assert.IsFalse(unreturnedKey.IsKeyReturned);
+
+            LogEntry returnedKey = new LogEntry
+            {
+                DateTimeIssued = DateTime.Now,
+                PersonDrawingKey = new Person { Name = "Drawer", ContactNumber = "90123456", NRIC = "100A", Rank = Rank.PTE },
+                PersonIssuingKey = new Person { Name = "Issuer", ContactNumber = "90123456", NRIC = "200B", Rank = Rank.CFC },
+                DateTimeReturned = DateTime.Now.AddMinutes(10),
+                PersonReturningKey = new Person { Name = "Drawer", ContactNumber = "90123456", NRIC = "100A", Rank = Rank.PTE },
+                PersonReceivingKey = new Person { Name = "Issuer", ContactNumber = "90123456", NRIC = "200B", Rank = Rank.CFC }
+            };
+            Assert.IsTrue(returnedKey.IsKeyReturned);
+        }
+
+
+
+    }
+}
