5c2cce42430c0702199fa997423d8c9c39a6b4cd
kaine119 <muikaien1@gmail.com>
2020-07-30 22:05:50 +0800

    Fix rank enum numbers

--- Database/DatabaseModels/Rank.cs
+++ Database/DatabaseModels/Rank.cs
@@ -11,47 +11,46 @@ namespace Database.DatabaseModels
         OTHER = 4,
 
         // Chao Recruits
-        REC = 4,
-        PTE = 5,
-        CPL = 6,
-        CFC = 7,
+        REC = 5,
+        PTE = 6,
+        CPL = 7,
+        CFC = 8,
 
         // Specs
-        THIRD_SGT = 8,
-        SECOND_SGT = 9,
-        FIRST_SGT = 10,
+        THIRD_SGT = 9,
+        SECOND_SGT = 10,
+        FIRST_SGT = 11,
 
-        STAFF_SGT = 11,
-        MASTER_SGT = 12,
+        STAFF_SGT = 12,
+        MASTER_SGT = 13,
 
         // Warrant Officers
-        THIRD_WO = 13,
-        SECOND_WO = 14,
-        FIRST_WO = 15,
+        THIRD_WO = 14,
+        SECOND_WO = 15,
+        FIRST_WO = 16,
 
-        MASTER_WO = 16,
-        SENIOR_WO = 17,
-        CHIEF_WO = 18,
+        MASTER_WO = 17,
+        SENIOR_WO = 18,
+        CHIEF_WO = 19,
 
         // Officers
-        SECOND_LT = 19,
-        FIRST_LT = 20,
-        CPT = 21,
+        SECOND_LT = 20,
+        FIRST_LT = 21,
+        CPT = 22,
 
         // Crabs
-        MAJ = 22,
-        LTC = 23,
-        SLTC = 24,
-        COL = 25,
+        MAJ = 23,
+        LTC = 24,
+        SLTC = 25,
+        COL = 26,
 
         // Stars
-        BG = 26,
-        MG = 27,
-        LG = 28,
-        GEN = 29,
+        BG = 27,
+        MG = 28,
+        LG = 29,
+        GEN = 30,
 
         // Military Experts
-        ME1 = 30,
         ME2 = 31,
         ME3 = 32,
         ME4 = 33,
@@ -59,9 +58,9 @@ namespace Database.DatabaseModels
         ME6 = 35,
         ME7 = 36,
         ME8 = 37,
+        ME9 = 38,
 
         // DXOs
-        DX1 = 38,
         DX2 = 39,
         DX3 = 40,
         DX4 = 41,
@@ -70,6 +69,7 @@ namespace Database.DatabaseModels
         DX7 = 44,
         DX8 = 45,
         DX9 = 46,
-        DX10 = 47
+        DX10 = 47,
+        DX11 = 48
     }
 }
