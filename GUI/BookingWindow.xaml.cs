9404005037cd0591898badb489e54c516412d393
kaine119 <muikaien1@gmail.com>
2020-08-13 18:08:49 +0800

    Add automatic time setting

--- GUI/BookingWindow.xaml.cs
+++ GUI/BookingWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using GUI.ViewModels;
+using System;
+using System.Timers;
+using System.Windows;
 
 namespace GUI
 {
@@ -7,13 +10,25 @@ namespace GUI
     /// </summary>
     public partial class BookingWindow : Window
     {
+        private readonly Timer timer = new Timer();
+
         public BookingWindow()
         {
             InitializeComponent();
+            // Use a timer to update the time field, and keep it up to date with current time.
+            timer.Elapsed += (_, e) =>
+            {
+                Dispatcher.Invoke(() => (DataContext as BookingViewModel).TimeIssued = new DateTimeOffset(e.SignalTime));
+            };
+            timer.AutoReset = true;
+            timer.Enabled = true;
         }
+
 
         private void Submit(object sender, RoutedEventArgs e)
         {
+            timer.Stop();
+            timer.Dispose();
             DialogResult = true;
         }
     }
