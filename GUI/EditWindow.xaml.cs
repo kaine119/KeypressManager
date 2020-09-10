140124fc2a3c66e3df587f83b002335a4d4e9b42
kaine119 <muikaien1@gmail.com>
2020-09-10 14:34:41 +0800

    Move parent EditWindow logic to a ViewModel

--- GUI/EditWindow.xaml.cs
+++ GUI/EditWindow.xaml.cs
@@ -8,27 +8,10 @@ namespace GUI
     /// </summary>
     public partial class EditWindow : Window
     {
-        private EditKeyBunchesViewModel editKeyVM = new EditKeyBunchesViewModel();
-        private EditStaffViewModel editStaffVM = new EditStaffViewModel();
-
-        public bool CanSave =>
-            editKeyVM.CmdSave.CanExecute();
-
         public EditWindow()
         {
             InitializeComponent();
-            EditKeyBunchesControl.DataContext = editKeyVM;
-            EditStaffControl.DataContext = editStaffVM;
-        }
-
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            if (CanSave)
-            {
-                editKeyVM.CmdSave.Execute(null);
-                editStaffVM.CmdSave.Execute(null);
-                DialogResult = true;
-            }
+            DataContext = new EditViewModel();
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
---
+++ GUI/ViewModels/EditViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+
+namespace GUI.ViewModels
+{
+    /// <summary>
+    /// Parent view model for the Edit Keypress window. Manages several
+    /// Edit component view models, and calls each component's Save command.
+    /// </summary>
+    class EditViewModel : INotifyPropertyChanged
+    {
+        private EditStaffViewModel _editStaffVM;
+
+        public EditStaffViewModel EditStaffVM
+        {
+            get { return _editStaffVM; }
+            set
+            {
+                _editStaffVM = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EditStaffVM"));
+            }
+        }
+
+        private EditKeyBunchesViewModel _editKeysVM;
+
+        public EditKeyBunchesViewModel EditKeyBunchesVM
+        {
+            get { return _editKeysVM; }
+            set
+            {
+                _editKeysVM = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EditKeysVM"));
+            }
+        }
+
+        public RelayCommand<EditWindow> CmdSave { get; set; }
+
+        public EditViewModel()
+        {
+            EditStaffVM = new EditStaffViewModel();
+            EditKeyBunchesVM = new EditKeyBunchesViewModel();
+
+            CmdSave = new RelayCommand<EditWindow>(
+                execute: (window) =>
+                {
+                    EditKeyBunchesVM.CmdSave.Execute(null);
+                    EditStaffVM.CmdSave.Execute(null);
+                    if (window != null)
+                    {
+                        window.DialogResult = true;
+                    }
+                },
+                canExecute: () => EditKeyBunchesVM.CmdSave.CanExecute() && EditStaffVM.CmdSave.CanExecute()
+            );
+
+            EditKeyBunchesVM.CmdSave.CanExecuteChanged += (_, __) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CmdSave"));
+            EditStaffVM.CmdSave.CanExecuteChanged      += (_, __) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CmdSave"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
